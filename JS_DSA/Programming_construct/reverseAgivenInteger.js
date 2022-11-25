f9225b2ee3ba1608f6c5ae52837e9816921244bf
lakharashubham007 <lakharashubham623@gmail.com>
2022-11-25 10:48:16 +0530

    dsa_JS

--- JS_DSA/Programming_construct/reverseAgivenInteger.js
+++ JS_DSA/Programming_construct/reverseAgivenInteger.js
@@ -1,12 +1,30 @@
 
 
+//JavaScript parseInt()
+/* Definition and Usage
+The parseInt method parses a value as a string and returns the first integer. */
 
-function reverseInt() {
+//solution1: reverse the number !
+const  num = 1002;
+const reverse = ( num ) => parseInt(String(num)
+        .split("")
+        .reverse()
+        .join(""),10);
 
-    let num = 123;
+console.log(reverse(num));
 
-    rem = num / 10;     
 
-    return rem;
+//solution2 : Iterative Way
+//TC: O(log n) SC: O(1)
+let nums = 4562;
+
+function reverseDigits(nums) {
+    let rev_num = 0;
+    while(nums > 0 ) {
+        rev_num = rev_num * 10 + nums % 10;
+        nums = Math.floor(nums/10);
+    }
+    return rev_num;
 }
 
+console.log(reverseDigits(nums));
