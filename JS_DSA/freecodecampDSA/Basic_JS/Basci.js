1eb5e4a362b624c5df53b3da76ad90e70712f724
lakharashubham007 <lakharashubham623@gmail.com>
2022-12-03 05:48:34 +0530

    DSA

---
+++ JS_DSA/freecodecampDSA/Basic_JS/Basci.js
@@ -0,0 +1,60 @@
+
+//1. Comment Your JavaScript Code
+
+// This is an in-line comment.
+
+/* This is a
+multi-line comment */
+/***************************************************************** */
+
+//2. Declare JavaScript Variables
+
+
+// info: JavaScript provides eight different data types which are :-> 
+//   undefined, null, boolean, string, symbol, bigint, number, and object.
+
+var  myName;
+let name;
+
+const start = 12; //variable is declared start = //value is initialized 12;
+
+/************************************************************* */
+
+//3.Storing Values with the Assignment Operator
+
+var a;
+a = 7;
+
+/*********************************************************** */
+
+//4. Assigning the Value of One Variable to Anothe
+
+var a = 7;
+var b;
+b = a;
+/*************************************************** */
+
+//5.  Initializing Variables with the Assignment Operator
+ 
+var a = 9;
+
+/***************************************** */
+
+//6. Declare String Variables
+
+var myFirstName = "Shubham";
+var myLastName = "Lakhara";
+
+/********************************************* */
+
+//7. Understanding Uninitialized Variables
+
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
